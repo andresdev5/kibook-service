3cfdd335afd951906fa51f597bb4e9ebb1a6da9a
Andrés Jácome <whitedev93@gmail.com>
2024-11-29 14:03:42 -0500

    Initial project

---
+++ src/main/java/ec/edu/espe/kibook/KibookServiceApplication.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.kibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibookServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KibookServiceApplication.class, args);
+	}
+}
